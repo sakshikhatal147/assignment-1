e366ccbe1a1d6c305dc8b88c9a512d80e13e3ea4
sakshikhatal147 <sakshikhatal1476@gmail.com>
2025-07-29 15:37:46 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+public class Array{Array
+    public static void main(String args[]){
+       int n,sum=0;
+    Scanner input = new Scanner(System.in);
+System.out.println("Enter the size:");
+
+      n=input.nextInt();
+     int arr[]=new int[n];
+System.out.println("Enter the numbers:");
+      for (int i=0;i<n;i++){
+       arr[i]=input.nextInt();
+      sum+=arr[i];
+    }
+System.out.println("Sum:"+sum);
+    
+}
+
+
+}
